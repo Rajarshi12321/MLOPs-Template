f78bda877de57c291046cffdf77ddb203b8a556c
Rajarshi Roy <royrajarshi0123@gmail.com>
2025-03-02 18:35:13 +0530

    first commit

--- demo_main.py
+++ demo_main.py
@@ -9,15 +9,3 @@ os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 ans = my_query("can you reccomend me some fantasy games? about 2 games")
 
 print(ans)
-
-
-echo "# h" >> README.md
-git init
-
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Rajarshi12321/Agentic-Career-Guide.git
-git push -u origin main
-
-git branch --set-upstream-to=origin/main main
